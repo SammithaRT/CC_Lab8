57c0f3df27a7dc17ed4c1777967605eae01c4b9b
SammithaRT <sammi.mrtk@gmail.com>
2025-03-18 15:00:19 +0530

    Changes made to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is Sammitha\n";
+  cout << "This is the secind change made\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
